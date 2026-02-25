ff6d14286bc5449c684c6aa96586ba0e2cb2fe1d
Luca Savio <savioluca2@gmail.com>
2026-02-25 11:12:14 +0700

    first commit

---
+++ components/auth/AuthNav.tsx
@@ -0,0 +1,9 @@
+export function AuthNav() {
+  return (
+    <nav className="auth-nav">
+      <a href="https://proselab.io" className="auth-nav-cta">
+        Open ProseLab
+      </a>
+    </nav>
+  )
+}
